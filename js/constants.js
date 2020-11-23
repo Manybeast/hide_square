effb80677d1e5ed3467343b768bfcaa70290f990
DmitriyIpatov <dmitriy.ipatov@together.com>
2020-11-23 13:56:25 +0200

    Added constants

---
+++ js/constants.js
@@ -0,0 +1,7 @@
+const BG_COLORS = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark'];
+const SIZES = [null, 'lg', 'md', 'sm'];
+const MAX_SQUARE = 24;
+let CREATING_LOOP = 20;
+let TIME_SECONDS = 60;
+
+export { BG_COLORS, SIZES, MAX_SQUARE, CREATING_LOOP, TIME_SECONDS };
--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
+import { BG_COLORS, SIZES, MAX_SQUARE, CREATING_LOOP, TIME_SECONDS } from './constants'
+
 (function ($) {
-  const bgColors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark'];
-  const sizes = [null, 'lg', 'md', 'sm'];
-  const maxSquare = 24;
-  let creatingLoop = 20;
-  let seconds = 60;
+  let creatingLoop = CREATING_LOOP;
+  let seconds = TIME_SECONDS;
   let started = false;
   let countdown = null;
   let currentResult = 0;
@@ -16,12 +15,13 @@
 
   function createSquare(color = 'transparent', point = 0) {
     const square = $('<div/>', {
-      'class': `_square square square-${sizes[point]} m-2 text-white h3 ${point && `border border-secondary rounded`} d-flex justify-content-center align-items-center bg-${color}`,
+      'class': `_square square square-${SIZES[point]} m-2 text-white h3 ${point && `border border-secondary rounded`} d-flex justify-content-center align-items-center bg-${color}`,
       'data-point': point
     }).html(point && point);
 
     return square;
   }
+
   function addSquares(times, newGame = false) {
     const container = $('._playingField');
 
@@ -29,10 +29,9 @@
       container.html('');
     }
 
-
     for (let i = 1; i <= times; i++) {
-      const point = randomInteger(1, sizes.length - 1);
-      const color = bgColors[randomInteger(0, bgColors.length - 1)];
+      const point = randomInteger(1, SIZES.length - 1);
+      const color = BG_COLORS[randomInteger(0, BG_COLORS.length - 1)];
 
       if(i % 3 === 0) {
         container.append(createSquare());
@@ -79,106 +78,114 @@
     timerContainer.html(strTimer);
   }
 
+  function handleStartClick() {
+    const playingField = $('._playingField');
+
+    if(started) {
+      clearInterval(countdown);
+      started = false;
+      $(this).html('Start');
+      ++seconds;
+
+      playingField.removeClass('active');
+
+      return;
+    }
+
+    playingField.addClass('active');
+
+    --seconds;
+
+    countdown = setInterval(function () {
+      if (seconds === 0) {
+        clearInterval(countdown);
+
+        $('#saveResult').modal();
+      } else {
+        timer(seconds);
+      }
+      --seconds;
+    }, 1000);
+
+    $(this).html('Pause');
+
+    started = true;
+  }
+
+  function handleNewGameClick() {
+    clearInterval(countdown);
+    seconds = TIME_SECONDS;
+    creatingLoop = CREATING_LOOP;
+    currentResult = 0;
+
+    $('._playingField').removeClass('active');
+    $('._point').html(currentResult);
+    $('._start').html('Start');
+    addSquares(creatingLoop, true);
+    timer(seconds)
+  }
+
+  function handleSquareClick() {
+    const _this = $(this);
+    const count = randomInteger();
+    const firstElem = $('._playingField').find('.square')[0];
+
+    if(_this.hasClass('square-null')) return;
+
+    currentResult = currentResult + _this.data('point');
+    --creatingLoop;
+
+    if($(firstElem).hasClass('square-null')) {
+      $(firstElem).remove();
+      --creatingLoop;
+    }
+
+    _this.remove();
+
+    if(creatingLoop + count < MAX_SQUARE) {
+      creatingLoop = creatingLoop + count;
+
+      addSquares(count);
+    }
+
+    $('._point').html(currentResult);
+  }
+
+  function setModalData() {
+    $('._savingResult').html(`${currentResult} points`);
+    $('input[name="player-name"]').focus();
+  }
+
+  function saveResult() {
+    const input = $('input[name="player-name"]');
+
+    setResult(input.val(), currentResult);
+
+    input.val('');
+
+    handleNewGameClick();
+    $('#saveResult').modal('hide');
+  }
+
+  function clearResult() {
+    localStorage.clear();
+    $('._reserved').remove();
+  }
+
   $(document).ready(function () {
     addSquares(creatingLoop);
     getResult();
 
-    $('._start').on('click', function () {
-      const playingField = $('._playingField');
+    $('._start').on('click', handleStartClick);
 
-      if(started) {
-        clearInterval(countdown);
-        started = false;
-        $(this).html('Start');
-        ++seconds;
+    $('._newGame, ._close').on('click', handleNewGameClick);
 
-        playingField.removeClass('active');
+    $('._playingField').on('click', '._square', handleSquareClick);
 
-        return;
-      }
+    $('#saveResult').on('show.bs.modal', setModalData);
 
-      playingField.addClass('active');
+    $('._save').on('click', saveResult);
 
-      --seconds;
-
-      countdown = setInterval(function () {
-        if (seconds === 0) {
-          clearInterval(countdown);
-
-          $('#saveResult').modal();
-        } else {
-          timer(seconds);
-        }
-        --seconds;
-      }, 1000);
-
-      $(this).html('Pause');
-
-      started = true;
-    });
-
-    $('._newGame').on('click', function () {
-      clearInterval(countdown);
-      seconds = 60;
-      creatingLoop = 10;
-      currentResult = 0;
-
-      $('._playingField').removeClass('active');
-      $('._point').html(currentResult);
-      $('._start').html('Start');
-      addSquares(creatingLoop, true);
-      timer(seconds)
-    });
-
-    $('._playingField').on('click', '._square', function (e) {
-      const _this = $(this);
-      const count = randomInteger();
-      const firstElem = $('._playingField').find('.square')[0];
-
-      if(_this.hasClass('square-null')) return;
-
-      currentResult = currentResult + _this.data('point');
-      --creatingLoop;
-
-      if($(firstElem).hasClass('square-null')) {
-        $(firstElem).remove();
-        --creatingLoop;
-      }
-
-      _this.remove();
-
-      if(creatingLoop + count < maxSquare) {
-        creatingLoop = creatingLoop + count;
-
-        addSquares(count);
-      }
-
-      $('._point').html(currentResult);
-    });
-
-    $('#saveResult').on('show.bs.modal', function () {
-      $('._savingResult').html(`${currentResult} points`);
-      $('input[name="player-name"]').focus();
-    });
-
-    $('._save').on('click', function () {
-      const input = $('input[name="player-name"]');
-
-      setResult(input.val(), currentResult);
-
-      input.val('');
-
-      $('._newGame').click();
-      $('#saveResult').modal('hide');
-    });
-
-    $('._close').on('click', function () {
-      $('._newGame').click();
-    });
-
-    $('._clear').on('click', function () {
-      localStorage.clear();
-      $('._reserved').remove();
-    });
+    $('._clear').on('click', clearResult);
   });
 })(jQuery);
